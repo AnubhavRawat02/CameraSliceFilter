b2e4f32240b98dd1eb3bb56033870cacb69a400e
anubhavTechrange <anubhav@techrange.co>
2024-11-30 22:27:32 +0530

    Initial Commit

---
+++ CameraSliceFilter/CameraSliceFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CameraSliceFilterApp.swift
+//  CameraSliceFilter
+//
+//  Created by Anubhav Rawat on 30/11/24.
+//
+
+import SwiftUI
+
+@main
+struct CameraSliceFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
